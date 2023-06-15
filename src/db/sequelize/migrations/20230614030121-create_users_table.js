9e6420740329a9f8ebe99ec51bec6362642b1472
Lucas <lucas_ccg@hotmail.com>
2023-06-15 15:37:26 -0300

    add first migration

--- src/db/sequelize/migrations/20230614030121-create_users_table.js
+++ src/db/sequelize/migrations/20230614030121-create_users_table.js
@@ -3,7 +3,49 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    await queryInterface.createTable('users',
+    { 
+      id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        primaryKey: true,
+        unique: true,
+      },
+      username:{
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      email:{
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password:{
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.ENUM("COMMON", "ADMIN"),
+        defaultValue: "COMMON",
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('now()'),
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('now()'),
+        allowNull: false,
+      },
+      removed_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('now()'),
+        allowNull: false,
+      }
+    });
   },
 
   async down (queryInterface, Sequelize) {

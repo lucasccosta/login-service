1b1f8f8bc20dc1729a5a98e791573e84040f9967
Lucas <lucas_ccg@hotmail.com>
2023-06-15 15:40:10 -0300

    add adapters and ports

---
+++ src/modules/account/adapters/index.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express'
+import { CreateUsersController } from '../controllers/create/CreateUsersController'
+
+export const adaptRoute = (createUsersController: CreateUsersController) => {
+  return async (request: Request, response: Response) => {
+
+    const httpResponse = await createUsersController.handle(request)
+
+    response.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}

e1e0fd86e7a73b26b2ba17ae022d5d7d09487f45
Lucas <lucas_ccg@hotmail.com>
2023-06-15 15:37:10 -0300

    changing connection string

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { app } from "./app";
 
 const PORT = 3333;
 
-const server = app.listen(PORT, () => console.log(`App running on port ${PORT}`));
+const server = app.listen(PORT, () => console.log(`App running at port http://locahost:${PORT}`));
 
 process.on("SIGINT", () =>{
   server.close();

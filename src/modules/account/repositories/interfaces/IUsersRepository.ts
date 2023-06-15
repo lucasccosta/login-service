64631511e562ef73b4c66667ffdd25acbcdf5af7
Lucas <lucas_ccg@hotmail.com>
2023-06-15 19:25:17 -0300

    changing reponses to user domain

--- src/modules/account/repositories/interfaces/IUsersRepository.ts
+++ src/modules/account/repositories/interfaces/IUsersRepository.ts
@@ -1,9 +1,9 @@
-import { UsersModel } from "../../../../db/sequelize/models/UserModel";
+import { User } from "../../domain/Users"; 
 import { ICreateUsersRequest } from "../../DTOs/IUsersDTO";
 
 export interface IUsersRepository {         
-  getUsers (): Promise<UsersModel[]>
-  getUserByEmail(email: string): Promise<UsersModel>
-  getUserByUsername(username: string): Promise<UsersModel>
-  createUser({username, email, password}: ICreateUsersRequest): Promise<UsersModel>
+  getUsers (): Promise<User[]>
+  getUserByEmail(email: string): Promise<User>
+  getUserByUsername(username: string): Promise<User>
+  createUser({username, email, password}: ICreateUsersRequest): Promise<User>
 }

04f96bdf8163aca4a916ceed1b6fec4f7423636b
Lucas <lucas_ccg@hotmail.com>
2023-06-15 15:37:46 -0300

    users model

--- src/db/sequelize/models/UserModel.ts
+++ src/db/sequelize/models/UserModel.ts
@@ -3,15 +3,53 @@ import { sequelize } from "../../../config/db-sequelize"
 
 class UsersModel extends Model {}
 
-UsersModel.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
+UsersModel.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    username:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM("COMMON", "ADMIN"),
+      defaultValue: "COMMON",
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false,
+    },
+    removed_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false,
+    }
+  }, 
+  {
+    tableName: "users",
+    timestamps: false,
+    sequelize
   }
-}, {
-  tableName: "user",
-  timestamps: false,
-  sequelize
-})
+  )
 
 export { UsersModel}

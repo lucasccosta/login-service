fe8f72889d3da266fa84f28d7093ba62186c8ece
Lucas <lucas_ccg@hotmail.com>
2023-06-16 13:58:59 -0300

    add password encrypt

--- src/modules/account/controllers/create/CreateUsersUseCase.ts
+++ src/modules/account/controllers/create/CreateUsersUseCase.ts
@@ -2,6 +2,7 @@ import { IUseCase } from "../../../../implementations/UseCase/IUseCase";
 import { databaseError } from "../../../../infra/http/helper";
 import { ICreateUsersRequest } from "../../DTOs/IUsersDTO";
 import { User } from "../../domain/Users";
+import * as bcrypt from "bcryptjs"
 import { IUsersRepository } from "../../repositories/interfaces/IUsersRepository";
 
 export class CreateUsersUseCase implements IUseCase {
@@ -13,16 +14,17 @@ export class CreateUsersUseCase implements IUseCase {
   async create(data: ICreateUsersRequest): Promise<User> {
     const { username, email, password } = data;
 
-    console.log("data: ", username, email, password)
     let userAlreadyExists = await this.usersRepository.getUserByEmail(email);
-    console.log("userAlready 1: ", userAlreadyExists)
     if(userAlreadyExists) throw databaseError("User already exists", 409)
     
     userAlreadyExists = await this.usersRepository.getUserByUsername(username);
-    console.log("userAlready : ", userAlreadyExists)
     if(userAlreadyExists) throw databaseError("User already exists", 409)
 
-    const user = await this.usersRepository.createUser({username, email, password})
+    const user = await this.usersRepository.createUser({
+      username,
+      email,
+      password: bcrypt.hashSync(password, 8)
+    })
 
     return user
   }
